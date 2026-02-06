e1946667e65ea9b15da90b3cc3a305fdfb547cb7
passiflora-brumaria <77532152+passiflora-brumaria@users.noreply.github.com>
2026-02-06 22:23:00 +0100

    Recurrence Field Added to Transaction Edit.

--- ui/pages/transaction_edit_page/transaction_edit_page.gd
+++ ui/pages/transaction_edit_page/transaction_edit_page.gd
@@ -44,6 +44,8 @@ func set_data (data: Dictionary) -> void:
 @onready var _submit_button: Button = $"./Stack/Submit"
 
 var _editable_model: Transaction
+var _add_recurrence: bool
+var _recurrence_type: ReocurringTransaction.Timeframe
 
 func _on_value_text_changed (_v: String) -> void:
 	_editable_model.value = Decimal.parse(_integer_value_field.text + "," + _decimal_value_field.text,",")
@@ -83,6 +85,23 @@ func _populate_date_field_options () -> void:
 	_date_month_field.selected = _date_month_field.get_item_index(_editable_model.date.month)
 	_date_year_field.text = str(_editable_model.date.year)
 
+func _on_recurrence_set (option_idx: int) -> void:
+	var option_id: int = $"./Stack/RecurrenceEdit".get_item_id(option_idx)
+	match option_id:
+		0:
+			_add_recurrence = false
+		1:
+			_add_recurrence = true
+			_recurrence_type = ReocurringTransaction.Timeframe.WEEK
+		2:
+			_add_recurrence = true
+			_recurrence_type = ReocurringTransaction.Timeframe.MONTH
+		3:
+			_add_recurrence = true
+			_recurrence_type = ReocurringTransaction.Timeframe.YEAR
+		_:
+			_add_recurrence = false
+
 func _on_submit_pressed () -> void:
 	if len(_category.transactions) == 0:
 		_category.transactions.push_back(_editable_model)
@@ -90,6 +109,16 @@ func _on_submit_pressed () -> void:
 		if _transaction != null:
 			var original_idx: int = _category.transactions.find(_transaction)
 			_category.transactions.remove_at(original_idx)
+		elif _add_recurrence:
+			var r := ReocurringTransaction.new()
+			r.blueprint = _editable_model.copy()
+			r.origin = _editable_model.date.copy()
+			r.next_application = _editable_model.date.copy()
+			r.timescale = _recurrence_type
+			var avoid_first_day := _editable_model.date.copy()
+			avoid_first_day.add_day()
+			r.create_appearences(avoid_first_day)
+			_category.recurring_transactions.push_back(r)
 		var insertion_idx: int = 0
 		while (insertion_idx < len(_category.transactions)) && _category.transactions.get(insertion_idx).date.is_prior_to(_editable_model.date):
 			insertion_idx += 1
@@ -101,10 +130,17 @@ func _on_submit_pressed () -> void:
 	queue_free()
 
 func _ready () -> void:
+	_add_recurrence = false
 	if _transaction != null:
 		_editable_model = _transaction.copy()
+		$"./Stack/DateMargin".queue_free()
+		$"./Stack/RecurrenceLabel".queue_free()
+		$"./Stack/RecurrenceGap".queue_free()
+		$"./Stack/RecurrenceEdit".queue_free()
 	else:
 		_editable_model = Transaction.new()
+		$"./Stack/RecurrenceEdit".select($"./Stack/RecurrenceEdit".get_item_index(0))
+		$"./Stack/RecurrenceEdit".item_selected.connect(_on_recurrence_set)
 	_category_icon.icon_name = _category.get_icon()
 	_category_icon.add_theme_color_override("font_color",_category_colour)
 	_category_name.text = _category.name
